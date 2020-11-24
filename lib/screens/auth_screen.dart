8a6736ef145e31d56229621d30186b47070ed534
pranavsrikanth <72306130+pranavsrikanth@users.noreply.github.com>
2020-11-24 14:20:55 +0530

    ps first

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -3,7 +3,7 @@ import "package:flutter/material.dart";
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+//this comment
 
 class AuthScreen extends StatefulWidget {
   @override
